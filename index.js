3f784ce668a3a8cc498777d7656b70f586f3c2fb
camozkan <145268753+camozkan@users.noreply.github.com>
2024-11-18 13:33:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ for (var i = 0; i<numberOfDrumButtons; i++) {
         var buttonInnerHTML = this.innerHTML;
 
         makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
         });
 }
 
 document.addEventListener("keydown", function (event){
    makeSound(event.key);
+   buttonAnimation(event.key);
 });
 
 
@@ -55,4 +58,16 @@ function makeSound(key) {
         default: console.log(buttonInnerHTML);
             
     }
+}
+
+
+function buttonAnimation(currentKey) {
+
+   var activeButton = document.querySelector("." + currentKey);
+
+   activeButton.classList.add("pressed");
+
+   setTimeout(function() {
+    activeButton.classList.remove("pressed");
+   }, 100);
 }
